4066614e9aa9e9eefd06423d0a6465748cfc280f
Arlette Xz ꒰🌷꒱ <angelcortes7r@gmail.com>
2025-12-28 11:35:46 -0600

    Actualizar _antilink.js

--- src/commands/_antilink.js
+++ src/commands/_antilink.js
@@ -1,21 +1,19 @@
 const linkRegex = /(https?:\/\/[^\s]+)|(www\.[^\s]+)/i;
 
-export async function before(m, { conn, isAdmin, isBotAdmin, isROwner }) {
-    if (!m.isGroup || !m?.text) return;
+export async function before(m, { conn, isAdmin, isBotAdmin }) {
+    if (!m.isGroup || !m.text) return;
 
-    const chat = global?.db?.data?.chats[m.chat];
+    const chat = global.db.data.chats[m.chat] || {};
     const isLink = linkRegex.test(m.text);
 
-    if (chat.antilink && isLink && !isAdmin && !isROwner) {
+    if (chat.antiLink && isLink) {
         if (!isBotAdmin) return;
+        if (isAdmin || m.fromMe) return; // Los admins no son expulsados
 
-        if (m.key.participant === conn.user.jid) return;
+        await conn.sendMessage(m.chat, { delete: m.key });
+        await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
 
-        await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: m.key.id, participant: m.key.participant }});
-
-        await conn.groupParticipantsUpdate(m.chat, [m.key.participant], 'remove');
-
-        const userName = global.db.data.users[m.key.participant]?.name || 'Usuario';
-        await conn.reply(m.chat, `> ꕥ Se ha eliminado a *${userName}* del grupo por \`Anti-Link\`. No se permiten enlaces de ningún tipo.`, null);
+        await conn.reply(m.chat, `ꕤ El usuario @${m.sender.split('@')[0]} ha sido eliminado por enviar enlaces. ✰`, null, { mentions: [m.sender] });
     }
+    return true;
 }
