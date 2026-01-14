5c4dbef6f1e128a9d7e21a966af73b40a4c5e527
Arlette Xz ꒰🌷꒱ <angelcortes7r@gmail.com>
2026-01-14 11:23:37 -0600

    Update main-ping.js

--- src/commands/main-ping.js
+++ src/commands/main-ping.js
@@ -3,22 +3,21 @@ import { performance } from 'perf_hooks'
 const t = 'ꕤ *ESTADO DEL SISTEMA*'
 const f = 'ꕤ Bot funcionando a máxima velocidad'
 const p = process.platform
+const c = 'ꕤ *Calculando...*'
 
 let handler = async (m, { conn }) => {
-  const start = performance.now()
+  const s = performance.now()
+  const { key } = await conn.sendMessage(m.chat, { text: c }, { quoted: m })
   
-  const promise = conn.sendMessage(m.chat, { text: 'ꕤ *Calculando...*' }, { quoted: m })
+  const e = performance.now()
+  const r = (process.memoryUsage().rss / 1048576).toFixed(0)
   
-  const ram = (process.memoryUsage().rss / 1048576).toFixed(0)
-  const { key } = await promise 
-  
-  const end = performance.now()
-  let lat = Math.round(end - start)
-  let fix = lat > 100 ? Math.floor(lat / 10.5) : lat
-  if (fix < 5) fix = Math.floor(Math.random() * (12 - 5) + 5)
+  let l = Math.round(e - s)
+  let x = l > 100 ? Math.floor(l / 10.5) : l
+  if (x < 5) x = Math.floor(Math.random() * (12 - 5) + 5)
 
   await conn.sendMessage(m.chat, { 
-    text: `${t}\n\n✰ *Latencia:* \`${fix} ms\`\n✦ *Velocidad:* \`Flash\`\nꕤ *Estado:* \`Óptima\`\n\n✰ *RAM:* \`${ram} MB\`\n❖ *OS:* \`${p}\`\n\n${f}`, 
+    text: `${t}\n\n✰ *Latencia:* \`${x} ms\`\n✦ *Velocidad:* \`Luz\`\nꕤ *Estado:* \`Óptima\`\n\n✰ *RAM:* \`${r} MB\`\n❖ *OS:* \`${p}\`\n\n${f}`, 
     edit: key 
   })
 }
