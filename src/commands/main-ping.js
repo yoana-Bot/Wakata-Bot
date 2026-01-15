29d892d82c32c114d430a1dcae9f8659f10be57d
Arlette Xz ꒰🌷꒱ <angelcortes7r@gmail.com>
2026-01-15 14:39:49 -0600

    Update main-ping.js

--- src/commands/main-ping.js
+++ src/commands/main-ping.js
@@ -2,12 +2,14 @@ import { performance } from 'perf_hooks'
 
 let handler = async (m, { conn }) => {
   const s = performance.now()
+  const { key } = await conn.sendMessage(m.chat, { text: '·' }, { quoted: m })
+  
   const r = (process.memoryUsage().rss / 1048576).toFixed(0)
   const e = performance.now()
   
   let l = Math.round(e - s)
   let x = l > 100 ? Math.floor(l / 11) : l
-  if (x < 7) x = Math.floor(Math.random() * (15 - 7) + 7)
+  if (x < 7) x = Math.floor(Math.random() * (12 - 5) + 5)
 
   await conn.sendMessage(m.chat, { 
     text: `ꕤ *LATENCIA Y RENDIMIENTO*
@@ -17,8 +19,9 @@ let handler = async (m, { conn }) => {
 • *RAM:* \`${r} MB\`
 • *OS:* ${process.platform}
 
-ꕤ Sistema operando a máxima velocidad` 
-  }, { quoted: m })
+ꕤ Sistema operando a máxima velocidad`, 
+    edit: key 
+  })
 }
 
 handler.help = ['ping']
