65428d7c78b690bc31972d9708c61e0edec1bd99
Arlette Xz ꒰🌷꒱ <angelcortes7r@gmail.com>
2026-01-15 14:33:25 -0600

    Speed

--- src/commands/main-status.js
+++ src/commands/main-status.js
@@ -2,27 +2,28 @@ import { cpus as _cpus, totalmem, freemem, platform, hostname } from 'os'
 import { sizeFormatter } from 'human-readable'
 
 const format = sizeFormatter({ std: 'JEDEC', decimalPlaces: 2, keepTrailingZeroes: false, render: (l, s) => `${l} ${s}B` })
-const arch = process.arch
-const plt = platform()
+const plt = `${platform()} ${process.arch}`
 const host = hostname().slice(0, 8)
 const cpuLen = _cpus().length
+const totalRam = format(totalmem())
 
 let handler = async (m, { conn }) => {
-    const usage = (100 - (100 * _cpus().reduce((a, c) => a + c.times.idle, 0) / _cpus().reduce((a, c) => a + Object.values(c.times).reduce((x, y) => x + y, 0), 0))).toFixed(1) + '%'
-
+    const db = global.db.data
+    const uptime = process.memoryUsage().rss
+    
     await conn.sendMessage(m.chat, { text: `「✦」Estado de *${global.botname}*
 
-❒ RAM [MAIN]: *${format(process.memoryUsage().rss)}*
-❒ CPU (x${cpuLen}): *${usage}*
-✿ Bots activos: *${global.conns.filter(c => c.user && c.ws?.socket?.readyState !== 3).length}*
-✐ Comandos: *${toNum(Object.values(global.db.data.users).reduce((a, u) => a + (u.commands || 0), 0))}*
-❒ Usuarios: *${Object.keys(global.db.data.users).length.toLocaleString()}*
-❒ Grupos: *${Object.keys(global.db.data.chats).length.toLocaleString()}*
-❒ Plugins: *${Object.values(global.plugins).filter(v => v.help && v.tags).length}*
+❒ RAM [MAIN]: *${format(uptime)}*
+❒ CPU (x${cpuLen}): *${(100 - (100 * _cpus().reduce((a, c) => a + c.times.idle, 0) / _cpus().reduce((a, c) => a + (c.times.user + c.times.nice + c.times.sys + c.times.irq + c.times.idle), 0))).toFixed(1)}%*
+✿ Bots activos: *${global.conns?.filter(c => c.user && c.ws?.socket?.readyState !== 3).length || 0}*
+✐ Comandos: *${toNum(Object.values(db.users).reduce((a, u) => a + (u.commands || 0), 0))}*
+❒ Usuarios: *${Object.keys(db.users).length.toLocaleString()}*
+❒ Grupos: *${Object.keys(db.chats).length.toLocaleString()}*
+❒ Plugins: *${Object.keys(global.plugins).length}*
 
 ◤ Sistema:
-    • *Plataforma:* ${plt} ${arch}
-    • *RAM Total:* ${format(totalmem())}
+    • *Plataforma:* ${plt}
+    • *RAM Total:* ${totalRam}
     • *RAM Usada:* ${format(totalmem() - freemem())}
     • *Host:* ${host}...` }, { quoted: m })
 }
