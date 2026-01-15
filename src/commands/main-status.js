5d30bdd89dbffa626f35b7cb2f98a7ded15053f5
Arlette Xz ꒰🌷꒱ <angelcortes7r@gmail.com>
2026-01-15 14:34:57 -0600

    Update main-status.js

--- src/commands/main-status.js
+++ src/commands/main-status.js
@@ -1,24 +1,21 @@
 import { cpus as _cpus, totalmem, freemem, platform, hostname } from 'os'
 import { sizeFormatter } from 'human-readable'
 
-const format = sizeFormatter({ std: 'JEDEC', decimalPlaces: 2, keepTrailingZeroes: false, render: (l, s) => `${l} ${s}B` })
-const plt = `${platform()} ${process.arch}`
-const host = hostname().slice(0, 8)
-const cpuLen = _cpus().length
-const totalRam = format(totalmem())
+const format = sizeFormatter({ std: 'JEDEC', decimalPlaces: 2, render: (l, s) => `${l} ${s}B` })
+const plt = `${platform()} ${process.arch}`, host = hostname().slice(0, 8), cpuLen = _cpus().length, totalRam = format(totalmem())
 
 let handler = async (m, { conn }) => {
-    const db = global.db.data
-    const uptime = process.memoryUsage().rss
+    const u = global.db.data.users
+    const c = global.db.data.chats
     
     await conn.sendMessage(m.chat, { text: `「✦」Estado de *${global.botname}*
 
-❒ RAM [MAIN]: *${format(uptime)}*
-❒ CPU (x${cpuLen}): *${(100 - (100 * _cpus().reduce((a, c) => a + c.times.idle, 0) / _cpus().reduce((a, c) => a + (c.times.user + c.times.nice + c.times.sys + c.times.irq + c.times.idle), 0))).toFixed(1)}%*
-✿ Bots activos: *${global.conns?.filter(c => c.user && c.ws?.socket?.readyState !== 3).length || 0}*
-✐ Comandos: *${toNum(Object.values(db.users).reduce((a, u) => a + (u.commands || 0), 0))}*
-❒ Usuarios: *${Object.keys(db.users).length.toLocaleString()}*
-❒ Grupos: *${Object.keys(db.chats).length.toLocaleString()}*
+❒ RAM [MAIN]: *${format(process.memoryUsage().rss)}*
+❒ CPU (x${cpuLen}): *${(100 - (100 * _cpus()[0].times.idle / Object.values(_cpus()[0].times).reduce((a, b) => a + b))).toFixed(1)}%*
+✿ Bots activos: *${global.conns?.length || 0}*
+✐ Comandos: *${toNum(Object.values(u).reduce((a, b) => a + (b.commands || 0), 0))}*
+❒ Usuarios: *${Object.keys(u).length}*
+❒ Grupos: *${Object.keys(c).length}*
 ❒ Plugins: *${Object.keys(global.plugins).length}*
 
 ◤ Sistema:
@@ -28,8 +25,6 @@ let handler = async (m, { conn }) => {
     • *Host:* ${host}...` }, { quoted: m })
 }
 
-handler.help = ['estado']
-handler.tags = ['main']
 handler.command = ['estado', 'status']
 handler.group = true
 
@@ -38,5 +33,5 @@ export default handler
 function toNum(n) {
     if (n >= 1000000) return (n / 1000000).toFixed(1) + 'M'
     if (n >= 1000) return (n / 1000).toFixed(1) + 'k'
-    return n.toString()
+    return n
 }
